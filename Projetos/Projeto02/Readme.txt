d27355289c58be8ac00a4d1c3e0fd9935545bb1a
Arthur-Carlos <arthur.carlos.dj@gmail.com>
2025-07-18 10:15:08 -0300

    projeto iniciado

---
+++ Projetos/Projeto02/Readme.txt
@@ -0,0 +1,1 @@
+Integrantes: Arthur Carlos Neuhauss
---
+++ Projetos/Projeto02/teste.c
@@ -0,0 +1,54 @@
+#include "stdio.h"
+#include "string.h"
+
+void teste(int r, int g, int b)
+{
+    printf("\x1b[38;2;%d;%d;%dm█", r, g, b);
+}
+
+int main(void)
+{
+    int i, j, x, y, z;
+    float r, g, b;
+
+    FILE *fp = fopen("gnomo.ppm", "rb");
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    char header[3];
+    int width, height, maxval;
+
+    fscanf(fp, "%2s", header);
+    if (strcmp(header, "P6") != 0)
+    {
+        printf("Formato PPM não suportado.\n");
+        fclose(fp);
+        return 1;
+    }
+    fscanf(fp, "%d %d %d", &width, &height, &maxval);
+    fgetc(fp);
+
+    unsigned char pixel[3];
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < width; j++)
+        {
+            fread(pixel, 1, 3, fp);
+            /*            
+            x = 255-pixel[0]; negativa
+            y = 255-pixel[1];
+            z = 255-pixel[2];
+             */
+            x = pixel[0];
+            y = pixel[1];
+            z = pixel[2];
+            teste(x,y,z);
+        }
+        printf("\n");
+    }
+    fclose(fp);
+    return 0;
+}
