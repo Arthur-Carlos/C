92f8a00128dff2be2b750382bae2a623feb9f868
Arthur Neuhauss <arthur.carlos.dj@gmail.com>
2025-07-24 23:30:29 -0300

    negativo/cinza feitos

--- Projetos/Projeto02/teste.c
+++ Projetos/Projeto02/teste.c
@@ -1,53 +1,65 @@
 #include <stdio.h>
-#include "string.h"
+#include <stdlib.h>
+#include <string.h>
 
-void teste(int r, int g, int b)
+unsigned char pixel[3];
+char header[3];
+int width, height, maxval;
+
+void converter_cinza(unsigned char *pixel)
 {
-    printf("\x1b[38;2;%d;%d;%dm█", r, g, b);
+    unsigned char cinza = (unsigned char)(0.3 * pixel[0] + 0.59 * pixel[1] + 0.11 * pixel[2]);
+    pixel[0] = pixel[1] = pixel[2] = cinza;
+}
+
+void converter_negativo(unsigned char *pixel)
+{
+    pixel[0] = maxval - pixel[0];
+    pixel[1] = maxval - pixel[1];
+    pixel[2] = maxval - pixel[2];
 }
 
 int main(void)
 {
-    int i, j, x, y, z;
+    FILE *fp_in = fopen("entrada.ppm", "rb");
+    FILE *fp_out = fopen("saida.ppm", "wb");
 
-    FILE *fp = fopen("gnomo.ppm", "rb");
-    if (fp == NULL)
+    if (fp_in == NULL || fp_out == NULL)
     {
-        printf("Erro ao abrir o arquivo.\n");
+        printf("Erro ao abrir os arquivos.\n");
         return 1;
     }
 
-    char header[3];
-    int width, height, maxval;
-
-    fscanf(fp, "%2s", header);
+    fscanf(fp_in, "%2s", header);
     if (strcmp(header, "P6") != 0)
     {
         printf("Formato PPM não suportado.\n");
-        fclose(fp);
+        fclose(fp_in);
+        fclose(fp_out);
         return 1;
     }
-    fscanf(fp, "%d %d %d", &width, &height, &maxval);
-    fgetc(fp);
 
-    unsigned char pixel[3];
-    for (i = 0; i < height; i++)
+    fscanf(fp_in, "%d %d %d", &width, &height, &maxval);
+    fgetc(fp_in);
+
+    fprintf(fp_out, "P6\n%d %d\n%d\n", width, height, maxval);
+
+    int total_pixels = width * height;
+    for (int i = 0; i < total_pixels; i++)
     {
-        for (j = 0; j < width; j++)
-        {
-            fread(pixel, 1, 3, fp);
-            /*            
-            x = 255-pixel[0]; negativa
-            y = 255-pixel[1];
-            z = 255-pixel[2];
-             */
-            x = pixel[0];
-            y = pixel[1];
-            z = pixel[2];
-            teste(x,y,z);
-        }
-        printf("\n");
+        fread(pixel, 1, 3, fp_in);
+
+        // Negativa
+        //converter_negativo(pixel);
+        // Cinza
+        //converter_cinza(pixel);
+        
+        fwrite(pixel, 1, 3, fp_out);
     }
-    fclose(fp);
+
+    fclose(fp_in);
+    fclose(fp_out);
+    printf("Imagem negativa salva como saida.ppm\n");
+
     return 0;
 }
